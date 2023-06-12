6173ad30992171edca014ca75448ef12fd5e0ff1
Nirshok <niro4ce@gmail.com>
2023-06-12 19:36:46 +0800

    File name is taken from input, simple test

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,26 @@
-
-pub struct Config {
-    pub file_path: String,
-}
-
-impl Config {
-    pub fn build(
-        mut args: impl Iterator<Item = String>
-    ) -> Result<Config, &'static str> {
-        args.next();
-
-        let file_path = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Didn't get a file path"),
-        };
-
-        Ok(Config{file_path})
-    }
-    
-}
-pub fn read_file(name: String) -> std::io::Result<String> {
+pub fn read_file(name: &str) -> std::io::Result<String> {
     let content = std::fs::read_to_string(name)?;
     Ok(content)
+}
+
+pub fn amount_of_lines(file: String) -> u64 {
+    let vec_str: Vec<&str> = file.split("\r").collect();
+    let mut number_of_lines: u64 = 0;
+    
+    for string in vec_str {
+        let mut single_line = from_string_to_numbers(string);
+
+        let original_len = single_line.len();
+        sort_and_remove_dups(&mut single_line); 
+        let dedup_len = single_line.len();
+
+        if original_len == dedup_len {
+            if min_max_sum_times_3_is_not_greater_than_sum_of_others_times_2(single_line) {
+                number_of_lines += 1;
+            }
+        }
+    };
+    number_of_lines
 }
 
 pub fn from_string_to_numbers(string: &str) -> Vec<i64> {
@@ -38,19 +38,23 @@ pub fn sort_and_remove_dups(vector: &mut Vec<i64>) {
     vector.dedup();
 }
 
-pub fn min_max_sum_times_3_is_not_greater_than_sum_of_others_times_2(numbers: Vec<i64>, lines: &mut u64) {
+pub fn min_max_sum_times_3_is_not_greater_than_sum_of_others_times_2(
+    numbers: Vec<i64>
+) -> bool {
     let first_number = numbers[0];
     let last_number = numbers[numbers.len() - 1];
 
-    let min_max_times_3 = min_max_times_3(first_number, last_number);
+    let min_max_times_3 = min_max_sum_times_3(first_number, last_number);
     let sum_others_times_2 = sum_others_times_2(first_number, last_number, numbers);
 
     if min_max_times_3 <= sum_others_times_2 {
-        *lines += 1;
+        return true;
+    } else {
+        return false;
     }
 }
 
-fn min_max_times_3(first: i64, last: i64) -> i64 {
+fn min_max_sum_times_3(first: i64, last: i64) -> i64 {
     (first + last) * 3
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,53 @@
 use ege_2::*;
-use std::{env, process};
-fn main() -> std::io::Result<()> {
-    // Should be a better way but can't find it atm,
-    // beacuse it gets os error 123 if taken from stdin()
-    let config = Config::build(env::args()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
+use std::io::stdin;
+fn main() {
+    let contents;
+    loop {
+        println!("Enter the file name:\n(If you want to quit, enter 'break'.)\n");
 
-    let contents = read_file(config.file_path)?;
-    let vec_str: Vec<&str> = contents.split("\r").collect();
-
-    let mut number_of_lines: u64 = 0;
-    
-    for string in vec_str {
-        let mut single_line = from_string_to_numbers(string);
-
-        let original_len = single_line.len();
-        sort_and_remove_dups(&mut single_line); 
-        let dedup_len = single_line.len();
-
-        if original_len == dedup_len {
-            min_max_sum_times_3_is_not_greater_than_sum_of_others_times_2(single_line, &mut number_of_lines);
+        let mut buff = String::new();
+        match stdin().read_line(&mut buff) {
+            Ok(_) => (),
+            Err(err) => {
+                eprintln!("\nError reading input: {err}\n");
+                continue;
+            }
         }
+        let name = buff.trim();
+
+        if name == "break" {
+            return println!("\nShutting down.");
+        }
+
+        contents = match read_file(name) {
+            Ok(content) => content,
+            Err(err) => {
+                eprintln!("\nError opening the file: {err}\n");
+                continue;
+            }
+        };
+        break;
     };
+
+    let lines = amount_of_lines(contents);
     
-    println!("{number_of_lines}");
-    Ok(())
+    println!("\nThe number of lines satisfying the condition is:\n{lines}")
 }
 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let file = String::from(
+            "99 55 66 55\r33 22 11 12\r4 5 6 7\r77 5 32 1\r99 98 2 15\r65 77 85 3\r11 12 21 121"
+        );
+
+        let lines = amount_of_lines(file);
+
+        assert_eq!(lines, 1);
+    }
+
+}
